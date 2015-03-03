d6dc61764ab73f4bcdca1280ae208353dba0757c
acatinfd <xiaorong@utexas.edu>
2015-03-03 00:17:12 -0600

    Create Petwork-iOS

---
+++ iOS/Petwork/Petwork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Petwork
+//
+//  Created by Xiaorong Zhu on 3/3/15.
+//  Copyright (c) 2015 Xiaorong Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

63a683f423542bc8a2e52275405c358351cfb738
acatinfd <xiaorong@utexas.edu>
2015-03-03 23:41:17 -0600

    Fixed the bug with PFQueryTableViewController
    Added the ParseUI.framework to fix the problem

---
+++ app0/Petwork/HomeViewController.h
@@ -0,0 +1,14 @@
+//
+//  HomeViewController.h
+//  Petwork
+//
+//  Created by Xiaorong Zhu on 3/3/15.
+//  Copyright (c) 2015 Xiaorong Zhu. All rights reserved.
+//
+
+#import <ParseUI/ParseUI.h>
+
+@interface HomeViewController : PFQueryTableViewController
+
+@end
+
